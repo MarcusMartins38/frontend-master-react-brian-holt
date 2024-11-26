c0e52eb384834f06e95293809b260b6542d3e565
unknown <marcusmartins38@gmail.com>
2024-11-26 20:17:59 -0300

    feat: server-side rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { lazy, Suspense, useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
@@ -20,31 +19,26 @@ const App = () => {
   const adoptedPetHook = useState(null);
 
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPetHook}>
-          <Suspense
-            fallback={
-              <div className="loading-pane">
-                <h2 className="loader">O</h2>
-              </div>
-            }
-          >
-            <header>
-              <Link to="/">Adopt Me!</Link>
-            </header>
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
-          </Suspense>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPetHook}>
+        <Suspense
+          fallback={
+            <div className="loading-pane">
+              <h2 className="loader">O</h2>
+            </div>
+          }
+        >
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </Suspense>
+      </AdoptedPetContext.Provider>
+    </QueryClientProvider>
   );
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
-root.render(<App />);
+export default App;
