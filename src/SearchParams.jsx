1981f396bd12139e1f6bcc633be557f38b0a5bb0
unknown <marcusmartins38@gmail.com>
2024-11-26 18:25:48 -0300

    feat: tailwind form plugin

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -20,8 +20,9 @@ export default function SearchParams() {
   const pets = results?.data?.pets ?? [];
 
   return (
-    <div className="search-params">
+    <div className="my-0 mx-auto w-11/12">
       <form
+        className="mb-10 flex flex-col items-center justify-center rounded-lg bg-gray-200 p-10 shadow-lg"
         onSubmit={(e) => {
           e.preventDefault();
           const formData = new FormData(e.target);
@@ -41,13 +42,21 @@ export default function SearchParams() {
         ) : null}
         <label htmlFor="location">
           Location
-          <input id="location" name="location" placeholder="Location" />
+          <input
+            id="location"
+            type="text"
+            name="location"
+            className="mb-5 block w-60"
+            placeholder="Location"
+          />
         </label>
 
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
+            name="animal"
+            className="mb-5 block w-60"
             value={animal}
             onChange={(e) => {
               setAnimal(e.target.value);
@@ -64,7 +73,12 @@ export default function SearchParams() {
 
         <label htmlFor="breed">
           Breed
-          <select id="breed" disabled={breeds.length === 0} name="breed">
+          <select
+            id="breed"
+            className="mb-5 block w-60 disabled:opacity-50"
+            disabled={breeds.length === 0}
+            name="breed"
+          >
             <option value=""></option>
             {breeds.map((breed) => (
               <option key={breed} value={breed}>
@@ -73,7 +87,9 @@ export default function SearchParams() {
             ))}
           </select>
         </label>
-        <button>Submit</button>
+        <button className="rounded border-none bg-orange-500 px-6 py-2 text-white hover:opacity-50">
+          Submit
+        </button>
       </form>
 
       <Results pets={pets} />
